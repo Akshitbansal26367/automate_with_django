7029864bcf91fc00a76ac2d34a106f745c84e883
Akshit Bansal <akshit08082003@gmail.com>
2025-07-18 11:12:43 +0530

    Add comment messages

--- dataentry/management/commands/helloworld.py
+++ dataentry/management/commands/helloworld.py
@@ -1,8 +1,12 @@
 from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
+    # This sets the description shown in "python manage.py help"
     help = "Prints Hello World"
     
     def handle(self, *args, **kwargs):
-        # we write the logic
+        """
+        This method runs automatically when the command is executed.
+        """
+        # Print "Hello World" to the console
         self.stdout.write("Hello World")
