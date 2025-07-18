3982dd142d824184036c9e63c12d001b7a622c30
Akshit Bansal <akshit08082003@gmail.com>
2025-07-18 11:25:21 +0530

    Add comments

--- dataentry/management/commands/greeting.py
+++ dataentry/management/commands/greeting.py
@@ -1,15 +1,26 @@
 from django.core.management.base import BaseCommand
 
-# Proposed command = python manage.py greeting name
-# Proposed output = Hi {name}, Good Morning
+# Custom management command: python manage.py greeting <name>
 class Command(BaseCommand):
-    help = "Greets the user"
+    help = "Greets the user"  # This shows up when you do python manage.py help
     
     def add_arguments(self, parser):
-        parser.add_argument("name", type=str, help="Specifies user name")
+        """
+        This method lets you add command line arguments to your command.
+        Here we're adding one positional argument: name
+        """
+        parser.add_argument(
+            "name",  # Positional argument (mandatory)
+            type=str,  # Type of the argument
+            help="Specifies user name"  # Help text
+        )
     
     def handle(self, *args, **kwargs):
-        # write the logic
-        name = kwargs['name']
+        """
+        This is the method that runs when your command is executed.
+        """
+        name = kwargs['name']  # Get the value of the 'name' argument
         greeting = f"Hi {name}, Good Morning!"
+        
+        # Output the result with success styling (green text in terminal)
         self.stdout.write(self.style.SUCCESS(greeting))
