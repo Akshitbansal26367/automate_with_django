2fe9c65adeb9d26f645f5a4b6a8d9b1f20725d53
Akshit Bansal <akshit08082003@gmail.com>
2025-07-20 09:45:06 +0530

    Add Customer model

--- dataentry/models.py
+++ dataentry/models.py
@@ -16,3 +16,16 @@ class Student(models.Model):
     def __str__(self):
         # When we print the Student object, it will show the student's name
         return self.name
+
+# Define a Customer model (this will create a table in the database named dataentry_customer)
+class Customer(models.Model):
+    # customer_name field: Stores the customer's name as a string (max length = 20 characters)
+    customer_name = models.CharField(max_length=20)
+    
+    # country field: Stores the customer's country as a string (max length = 20 characters)
+    country = models.CharField(max_length=20)
+    
+    # String representation of the Customer object (useful in admin panel and shell)
+    def __str__(self):
+        # When we print the Customer object, it will show the customer's name
+        return self.customer_name
