c163fdc9da98da6777a774b957e8747c1504fe9c
Akshit Bansal <akshit08082003@gmail.com>
2025-07-28 19:58:57 +0530

    login registration feature

--- awd_main/settings.py
+++ awd_main/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "dataentry",
     "uploads",
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -147,3 +149,5 @@ EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = "Automate with Django <fakeakshitid@gmail.com>"
 DEFAULT_TO_EMAIL = "akshit246bansal@gmail.com"
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
